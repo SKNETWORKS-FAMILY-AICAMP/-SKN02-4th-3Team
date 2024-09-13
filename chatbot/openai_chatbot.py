328e8ecc92b1c9e8f2715b7d3e60c42e27b8a70e
WOOK <wjstkddnr020@gmail.com>
2024-09-13 11:42:16 +0900

    openai_chatbot.py update

--- chatbot/openai_chatbot.py
+++ chatbot/openai_chatbot.py
@@ -1,30 +1,56 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.utilities import SQLDatabase
-from langchain_community.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma 
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
 from langchain.docstore.document import Document
 from langchain.prompts import PromptTemplate
 from langchain.schema.runnable import RunnablePassthrough
+import pymysql
 
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-host = os.getenv('host')
+host = os.getenv('DB_HOST')
 port = os.getenv('port')
-username = os.getenv('username')
-password = os.getenv('password')
-database_schema = os.getenv('database_schema')
-openai_api_key = os.getenv('OPENAI_API_KEY')
+username = os.getenv('DB_USER')
+password = os.getenv('DB_PASSWORD')
+database_schema = os.getenv('DB_NAME')
+openai_api_key = os.getenv('OPEN_API_KEY')
 
+def db_connect():
+    conn = pymysql.connect(
+        host=host,
+        user=username,
+        password=password,
+        database=database_schema,
+        charset="utf8"
+    )
+    return conn
+
+def fetch_long_text():
+    try:
+        conn = db_connect()
+        cursor = conn.cursor()
+        cursor.execute('SELECT txt FROM naver_finance ORDER BY time DESC LIMIT 1')
+        result = cursor.fetchone()
+        long_text = result[0] if result else None
+        cursor.close()
+        conn.close()
+        return long_text
+    except pymysql.Error as e:
+        print(f"Error: {e}")
+        return None
 def url_to_db(username,password,host,port,database_schema):
     mysql_uri = f"mysql+pymysql://{username}:{password}@{host}:{port}/{database_schema}?charset=utf8"
     db = SQLDatabase.from_uri(mysql_uri)
-    db1=db.run("SELECT * FROM news WHERE DATE(time) = CURDATE();;")
-    db2=db.run("SELECT * FROM naver_finance WHERE DATE(time) = CURDATE();")
-    db3=db1+db2
+    db1=db.run("SELECT * FROM news WHERE DATE(time) = CURDATE();")
+    db2=fetch_long_text()
+    db3=db2+db1
+    with open('output.txt', 'w', encoding='utf-8') as f:
+        f.write(db2)
     return db3
 
 def split_text(db, chunk_size=500, chunk_overlap=0):
@@ -38,6 +64,7 @@ def create_vector_store(splits, OPENAI_API_KEY):
     """문서를 벡터로 변환하여 Vector Store를 생성하고 저장하는 함수."""
     # Document 객체 리스트 생성
     documents = [Document(page_content=split, metadata={}) for split in splits]
+    ids = [f"doc{i+1}" for i in range(len(documents))]
 
     # OpenAI Embeddings 초기화
     embedding = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
@@ -46,9 +73,12 @@ def create_vector_store(splits, OPENAI_API_KEY):
     vectorstore = Chroma.from_documents(
         documents=documents,
         embedding=embedding,
+        ids=ids,
         persist_directory="./chroma_db"  # 저장될 디렉토리 지정
     )
     
+    vectorstore.persist()
+
     # 벡터 스토어를 검색 가능한 리트리버로 설정
     retriever = vectorstore.as_retriever()
     return retriever
@@ -56,13 +86,9 @@ def create_vector_store(splits, OPENAI_API_KEY):
 def create_prompt_template():
     """곤란한 질문에도 적절한 답변을 제공하는 커스텀 프롬프트 템플릿을 생성하는 함수."""
     template = """
-    주식이나 뉴스, 증시에 관한 내용이 아니면 잘 알 수 없는 내용이니 대답할 수 없다고 말하십시오.
-    질문에 대해 가능한 한 최선의 답변을 제공하되, 정확하지 않거나 모호한 부분은 솔직히 인정하십시오.
-    주식의 경우 관련 도메인이나 테마를 언급해줘.
-    정답을 확신할 수 없는 경우 '이 부분은 정확히 알 수 없습니다.'라고 명확히 말하십시오.
-    답변을 단계적으로 생각하고 필요한 순서를 정한 후, 그 순서에 따라 자세히 링크가 있으면 각각 다른 링크를 첨부해서 설명해 주십시오.
+    기본적으로 네이버 파이낸스에 있는 정보를 토대로 대답하십시오.
+    뉴스에 대한 질문에는 뉴스 제목, 요약, 링크를 첨부해서 설명해 주십시오.
     언제나 '모든 투자에 대한 책임은 본인에게 있습니다.'이라고 답변 끝에 적어주세요.
-    계산 문제는 반드시 단계별로 과정을 보여주십시오.
     대답할 때 상대방을 존중하고 예의를 갖추어 답변해 주세요.
     {context}
     질문: {question}
